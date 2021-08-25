cd336620cd8355a36455d42e042375d291c85f05
lyauy <thibaut.yauy@gmail.com>
2021-08-25 14:57:45 +0200

    API revisité en prenant en compte les épreuves et résultats

--- API/index.js
+++ API/index.js
@@ -11,11 +11,33 @@ app.get('/sports', (req,res) => {
 })
 
 //Lecture d'un sport
-app.get('/sports/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    const sport = sports.find(sports => sports.id === id)
+app.get('/sports/:id_sport', (req,res) => {
+    const id = req.params.id_sport
+    const sport = sports.find(sports => sports.id_sport === id)
     res.status(200).json(sport)
 })
+
+//Lecture des épreuves d'un sport
+app.get('/sports/:id_sport/epreuves', (req,res) => {
+    const id = req.params.id_sport
+    const sport = sports.find(sports => sports.id_sport === id)
+    res.status(200).json(sport.epreuves)
+})
+
+//Lecture d'une épreuve
+app.get('/sports/:id_sport/epreuves/:id_epreuve', (req,res) => {
+    const id_sport = req.params.id_sport
+    const id_epreuve = req.params.id_epreuve
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    const epreuve = epreuves.find(epreuves => epreuves.id_epreuve === id_epreuve)
+    res.status(200).json(epreuve)
+})
+
+
+
+
+
 
 //Nouveau sport
 app.post('/sports', (req,res) => {
@@ -23,23 +45,84 @@ app.post('/sports', (req,res) => {
     res.status(200).json(sports)
 })
 
-//Modification d'un sport
-app.patch('/sports/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    let sport = sports.find(sport => sport.id === id)
-    sport.epreuve = req.body.epreuve,
-    sport.lieu = req.body.lieu,
-    sport.date = req.body.date,
-    sport.horaire = req.body.horaire,
-    res.status(200).json(sport)
+//Nouvelle épreuve
+app.post('/sports/:id_sport/epreuves', (req,res) => {
+    const id_sport = req.params.id_sport
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    epreuves.push(req.body)
+    res.status(200).json(epreuves)
 })
 
+//Nouveau résultat
+app.post('/sports/:id_sport/epreuves/:id_epreuve/resultats', (req,res) => {
+    const id_sport = req.params.id_sport
+    const id_epreuve = req.params.id_epreuve
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    const epreuve = epreuves.find(epreuves => epreuves.id_epreuve === id_epreuve)
+    const resultats = epreuve.resultats
+    resultats.push(req.body)
+    res.status(200).json(resultats)
+})
+
+
+
+
+
+//Modification d'une épreuve
+app.patch('/sports/:id_sport/epreuves/:id_epreuve', (req,res) => {
+    const id_sport = req.params.id_sport
+    const id_epreuve = req.params.id_epreuve
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    const epreuve = epreuves.find(epreuves => epreuves.id_epreuve === id_epreuve)
+    epreuve.lieu = req.body.lieu,
+    epreuve.date = req.body.date,
+    epreuve.horaire = req.body.horaire,
+    res.status(200).json(epreuve)
+})
+
+
+
+
+
+
 //suppression d'un sport
-app.delete('/sports/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    let sport = sports.find(parking => parking.id === id)
+app.delete('/sports/:id_sport', (req,res) => {
+    const id = req.params.id_sport
+    let sport = sports.find(sport => sport.id_sport === id)
     sports.splice(sports.indexOf(sport),1)
     res.status(200).json(sports)
 })
+
+//suppression d'une épreuve
+app.delete('/sports/:id_sport/epreuves/:id_epreuve', (req,res) => {
+    const id_sport = req.params.id_sport
+    const id_epreuve = req.params.id_epreuve
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    let epreuve = epreuves.find(epreuve => epreuve.id_epreuve === id_epreuve)
+    epreuves.splice(epreuves.indexOf(epreuve),1)
+    res.status(200).json(epreuves)
+})
+
+//suppression d'un résultat
+app.delete('/sports/:id_sport/epreuves/:id_epreuve/resultats/:id_resultat', (req,res) => {
+    const id_sport = req.params.id_sport
+    const id_epreuve = req.params.id_epreuve
+    const id_resultat = req.params.id_resultat
+    const sport = sports.find(sports => sports.id_sport === id_sport)
+    const epreuves = sport.epreuves
+    const epreuve = epreuves.find(epreuves => epreuves.id_epreuve === id_epreuve)
+    const resultats = epreuve.resultats
+    let resultat = resultats.find(resultat => resultat.id_resultat === id_resultat)
+    resultats.splice(resultats.indexOf(resultat),1)
+    res.status(200).json(resultats)
+})
+
+
+
+
 
 app.listen(8080, () => {})
